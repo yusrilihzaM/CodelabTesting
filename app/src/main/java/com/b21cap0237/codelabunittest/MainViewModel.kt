809d51e497071992ec0ff6c3be7e12b3db7db206
Moh Yusril Ihza Maulana <mohyusrilihzamaulana@gmail.com>
2021-05-17 15:35:09 +0700

    Unit Testing

---
+++ app/src/main/java/com/b21cap0237/codelabunittest/MainViewModel.kt
@@ -0,0 +1,10 @@
+package com.b21cap0237.codelabunittest
+
+class MainViewModel(private val cuboidModel: CuboidModel) {
+    fun getCircumference() = cuboidModel.getCircumference()
+    fun getSurfaceArea() = cuboidModel.getSurfaceArea()
+    fun getVolume() = cuboidModel.getVolume()
+    fun save(w: Double, l: Double, h: Double) {
+        cuboidModel.save(w, l, h)
+    }
+}
